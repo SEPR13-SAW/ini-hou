856aad3bfed7f6d4031315f3c745ff5395209916
juventietis <mantasmarkeviciuslt@gmail.com>
2014-04-05 13:24:23 +0100

    landed planes appear next to the airport. WIP

--- src/main/java/seprini/models/Airport.java
+++ src/main/java/seprini/models/Airport.java
@@ -1,20 +1,26 @@
 package seprini.models;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import seprini.data.Art;
 
 public final class Airport extends Entity{
-
 	Waypoint runwayEnd;
 	Waypoint runwayMid;
 	Waypoint runwayStart;
 	Waypoint approach;
 	Queue<Aircraft> landedPlanes = new LinkedList<Aircraft>();
+	TextureRegion aircraft;
+	float[] landedPositionsX = new float[10];
+	float[] landedPositionsY = new float[10];
+	
+	
 	
 	private final boolean visible = true;
 	private int id;
@@ -27,10 +33,28 @@ public final class Airport extends Entity{
 		this.debugShape = true;
 		this.coords = new Vector2(midPoint.x, midPoint.y);
 		this.size = new Vector2(154, 120);
-		this.texture = Art.getTextureRegion("airport");	
+		this.texture = Art.getTextureRegion("airport");
+		this.aircraft = Art.getTextureRegion("aircraft");
 		this.id = ID;
+		this.adjustPositions();
 	}
 	
+	public void adjustPositions(){
+		int offsetX = 40, offsetY = 25, stepY = 20;
+		for(int i = 0; i < 5; i++){
+			this.landedPositionsX[i] = this.runwayStart.getX() + offsetX;
+			this.landedPositionsY[i] = this.runwayStart.getY() + offsetY;
+			offsetY += stepY;
+		};
+		offsetY = 5;
+		offsetX = - 35;
+		
+		for(int o = 5; o < 10; o++){
+			this.landedPositionsX[o] = this.runwayStart.getX() + offsetX;
+			this.landedPositionsY[o] = this.runwayStart.getY() + offsetY;
+			offsetY += stepY;
+		}
+	}
 	
 	public int getID(){
 		return this.id;
@@ -66,6 +90,11 @@ public final class Airport extends Entity{
 	public void draw(SpriteBatch batch, float parentAlpha) {
 		if (visible){
 			super.draw(batch, parentAlpha);
+			for(int i = 0; i < this.landedPlanes.size(); i++)
+				if(i < 5)
+					batch.draw(aircraft, this.landedPositionsX[i], this.landedPositionsY[i], 0, 0, aircraft.getRegionWidth(), aircraft.getRegionHeight(), 0.3f, 0.3f, 160f);
+				else
+					batch.draw(aircraft, this.landedPositionsX[i], this.landedPositionsY[i], 0, 0, aircraft.getRegionWidth(), aircraft.getRegionHeight(), 0.3f, 0.3f, 20f);
 		}
 	}
 
