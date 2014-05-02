7e4401b0de8cfd1538d08fb878a1a3d2b45ab09d
mantasmarkeviciuslt@gmail.com <mantasmarkeviciuslt@gmail.com>
2014-05-02 13:12:36 +0100

    start multiplayer screen wip

---
+++ src/main/java/seprini/controllers/StartMultiplayerController.java
@@ -0,0 +1,80 @@
+package seprini.controllers;
+
+import java.util.HashMap;
+
+import seprini.ATC;
+import seprini.data.Art;
+import seprini.data.GameDifficulty;
+import seprini.screens.MenuScreen;
+import seprini.screens.StartMultiplayerScreen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.esotericsoftware.tablelayout.Cell;
+
+public class StartMultiplayerController extends ChangeListener {
+
+	private final Table ui;
+	private final StartMultiplayerScreen screen;
+
+	private HashMap<String, TextButton> buttons;
+
+	public StartMultiplayerController(StartMultiplayerScreen screen, Table ui) {
+		this.ui = ui;
+		// Moves ui down, to leave space for buttons
+		ui.setPosition(0, -30);
+		this.screen = screen;
+
+		buttons = new HashMap<String, TextButton>();
+		addWidgets();
+
+	}
+
+	private void addWidgets() {
+		Label enterPlayerName = new Label("Please enter your player name:", Art.getSkin());
+		ui.add(enterPlayerName);
+		ui.row();
+		TextField playerName = new TextField("Enter you name", Art.getSkin());
+		playerName.addListener(this);
+		playerName.setScale(2);
+		ui.add(playerName).height(50).width(100);
+
+	}
+
+	/**
+	 * Convinience method to add a button to the UI
+	 * 
+	 * @param name
+	 * @param text
+	 * @return
+	 */
+	private Cell<?> addButton(String name, String text, ChangeListener listener) {
+		TextButton button = new TextButton(text, Art.getSkin());
+		buttons.put(name, button);
+		button.addListener(listener);
+
+		return ui.add(button);
+	}
+
+	@Override
+	public void changed(ChangeEvent event, Actor actor) {
+
+		// stop the anthem after entering the game
+		Art.getSound("comeflywithme").stop();
+		Art.getSkin().getFont("default").setScale(1f);
+
+		// Pass difficulty to the newly created GameScreen so the game can
+		// change variables depending on it
+
+		if (actor.equals(buttons.get("exit")))
+			Gdx.app.exit();
+	}
+
+}
